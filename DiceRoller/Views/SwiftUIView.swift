5a134d4000c23ccce60f24d38a60a144721985a7
Julien Hwang <jhwang@lcs.on.ca>
2025-11-12 10:25:51 -0500

    Made the view to be able to change number of dices easily

--- DiceRoller/Views/SwiftUIView.swift
+++ DiceRoller/Views/SwiftUIView.swift
@@ -8,18 +8,21 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State private var numberOfDice: Int = 1
+    
     var body: some View {
         VStack {
             Text("Dice Roller")
                 .font(.largeTitle.lowercaseSmallCaps())
             
             HStack {
-                DiceView()
-                DiceView()
-                DiceView()
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                    DiceView()
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
