3507d1d9b8240680d15a6b5ea1a54b57425ce0c3
Julien Hwang <jhwang@lcs.on.ca>
2025-11-12 09:51:35 -0500

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Julien Hwang on 12/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
